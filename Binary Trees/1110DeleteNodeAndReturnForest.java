c9a9f968aaa907f9731d6c28ff5594f9a11a7832
rachit-walia <rachitwalia3@gmail.com>
2025-04-28 23:49:27 +0530

    1102

---
+++ Binary Trees/1110DeleteNodeAndReturnForest.java
@@ -0,0 +1,42 @@
+
+// 1110. Delete Nodes And Return Forest
+class Solution {
+
+    private TreeNode deleteHelper(TreeNode root, Set<Integer> toDelete, List<TreeNode> result) {
+        if (root == null) {
+            return null;
+        }
+
+        root.left = deleteHelper(root.left, toDelete, result);
+        root.right = deleteHelper(root.right, toDelete, result);
+
+        if (toDelete.contains(root.val)) {
+            if (root.left != null) {
+                result.add(root.left);
+            }
+            if (root.right != null) {
+                result.add(root.right);
+            }
+            return null;
+        } else {
+            return root;
+        }
+    }
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        List<TreeNode> result = new ArrayList<>();
+        Set<Integer> toDelete = new HashSet<>();
+
+        for (int num : to_delete) {
+            toDelete.add(num);
+        }
+
+        deleteHelper(root, toDelete, result);
+
+        if (!toDelete.contains(root.val)) {
+            result.add(root);
+        }
+
+        return result;
+    }
+}
