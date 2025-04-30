7213248fe99f07d90d2eacb1417e6153b2b494a4
rachit-walia <rachitwalia3@gmail.com>
2025-04-30 22:37:15 +0530

    113

---
+++ Binary Trees/113PathSumII.java
@@ -0,0 +1,28 @@
+// Path Sum II
+
+public class Solution {
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        collectPaths(root, sum, temp, result);
+        return result;
+    }
+
+    private void collectPaths(TreeNode root, int curr, List<Integer> temp, List<List<Integer>> result) {
+        if (root == null) {
+            return;
+        }
+
+        temp.add(root.val);
+
+        if (root.left == null && root.right == null && root.val == curr) {
+            result.add(new ArrayList<>(temp));
+        }
+
+        collectPaths(root.left, curr - root.val, temp, result);
+        collectPaths(root.right, curr - root.val, temp, result);
+
+        temp.remove(temp.size() - 1);
+    }
+}
