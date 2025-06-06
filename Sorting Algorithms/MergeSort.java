04d1dd7484a5d560a3b3e7b52425b44ee9e43835
rachit-walia <rachitwalia3@gmail.com>
2025-06-06 22:15:18 +0530

    mergeSort

---
+++ Sorting Algorithms/MergeSort.java
@@ -0,0 +1,56 @@
+
+public class MergeSort {
+
+    // Merge two sorted subarrays: arr[l..mid] and arr[mid+1..r]
+    static void merge(int[] arr, int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[mid + 1 + j];
+        }
+
+        // Merge the temp arrays
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+        // Copy remaining elements
+        while (i < n1) {
+            arr[k++] = left[i++];
+        }
+        while (j < n2) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    // Divide and sort
+    static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int mid = l + (r - l) / 2;
+            mergeSort(arr, l, mid);       // Sort left half
+            mergeSort(arr, mid + 1, r);   // Sort right half
+            merge(arr, l, mid, r);        // Merge them
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 63, 23, 56, 22};
+        mergeSort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
