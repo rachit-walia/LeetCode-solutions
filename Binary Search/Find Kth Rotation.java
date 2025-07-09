db83deef385085a2d92a7ab74173a0704ebfe17b
rachit-walia <rachitwalia3@gmail.com>
2025-07-09 15:00:07 +0530

    kth rotation

---
+++ Binary Search/Find Kth Rotation.java
@@ -0,0 +1,37 @@
+//  Find Kth Rotation
+/*
+ 
+Given an increasing sorted rotated array arr of distinct integers. The array is right-rotated k times. Find the value of k.
+Let's suppose we have an array arr = [2, 4, 6, 9], so if we rotate it by 2 times so that it will look like this:
+After 1st Rotation : [9, 2, 4, 6]
+After 2nd Rotation : [6, 9, 2, 4]
+
+Examples:
+
+Input: arr = [5, 1, 2, 3, 4]
+Output: 1
+Explanation: The given array is 5 1 2 3 4. The original sorted array is 1 2 3 4 5. We can see that the array was rotated 1 times to the right.
+Input: arr = [1, 2, 3, 4, 5]
+Output: 0
+Explanation: The given array is not rotated.
+
+ */
+
+class Solution {
+
+    public int findKRotation(List<Integer> arr) {
+        int s = 0;
+        int e = arr.size() - 1;
+
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (arr.get(mid) > arr.get(e)) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+
+        return s;
+    }
+}
