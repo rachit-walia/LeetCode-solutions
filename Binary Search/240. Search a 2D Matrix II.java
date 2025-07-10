c803d52650194a2d4aba4c57d0eb78782db11aff
rachit-walia <rachitwalia3@gmail.com>
2025-07-10 13:06:49 +0530

    240

---
+++ Binary Search/240. Search a 2D Matrix II.java
@@ -0,0 +1,49 @@
+// 240. Search a 2D Matrix II
+
+/* Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false */
+class Solution {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (target >= matrix[i][0] && target <= matrix[i][matrix[i].length - 1]) {
+                if (binarySearch(matrix[i], target)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean binarySearch(int[] row, int target) {
+        int left = 0;
+        int right = row.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
