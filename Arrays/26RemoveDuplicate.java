8b4b71fc3e1f828d2e09fad7f4113c12ecf92960
rachit-walia <rachitwalia3@gmail.com>
2025-07-11 21:31:14 +0530

    26

--- Arrays/26RemoveDuplicate.java
+++ Arrays/26RemoveDuplicate.java
@@ -15,3 +15,21 @@ class Solution {
         return i + 1;
     }
 }
+
+//  Method 2
+class Solution {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int result = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[result] = nums[i];
+                result++;
+            }
+        }
+        return result;
+    }
+}
